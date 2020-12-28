48c55e0d9b0e368b953aa69dfa7521a8d6f5572b
Duc Do <ddo@beyond.ai>
2020-12-28 11:13:58 -0600

    test9

--- packages/regression_model/tests/test_predict.py
+++ packages/regression_model/tests/test_predict.py
@@ -14,18 +14,18 @@ def test_simple():
     assert 2 == 2
 
 
-# def test_make_single_prediction():
-#     # Given
-#     test_data = load_dataset(file_name="test.csv")
-#     single_test_json = test_data[0:1]
+def test_make_single_prediction():
+    # Given
+    test_data = load_dataset(file_name="test.csv")
+    single_test_json = test_data[0:1]
 
-#     # When
-#     subject = make_prediction(input_data=single_test_json)
+    # When
+    subject = make_prediction(input_data=single_test_json)
 
-#     # Then
-#     assert subject is not None
-#     assert isinstance(subject.get("predictions")[0], float)
-#     assert math.ceil(subject.get("predictions")[0]) == 112476
+    # Then
+    assert subject is not None
+    assert isinstance(subject.get("predictions")[0], float)
+    assert math.ceil(subject.get("predictions")[0]) == 112476
 
 
 # def test_make_multiple_predictions():
