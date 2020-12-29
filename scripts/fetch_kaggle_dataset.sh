932e6c4004aee8b6cddea4e88fb25cfed29ce9de
Duc Do <ddo@beyond.ai>
2020-12-29 14:17:21 -0600

    changed fetch kaggle

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+# 1. use kaggle API to download the dataset from kaggle website and Save the dataset in the designated dataset directory in the package
+
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
+
+#2. unzip the downloaded folder
+unzip  packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
+
+#3. pick only the interested files and put them in the package dataset
+mv train.csv  packages/regression_model/regression_model/datasets/
+mv test.csv  packages/regression_model/regression_model/datasets/
